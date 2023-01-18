3f9f325b0f87d571dd8dc89341731e5421e580a7
sord-dev <75338985+sord-dev@users.noreply.github.com>
2023-01-18 22:19:55 +0000

    adding new link and fixing typo for link noreferrer

--- src/components/linkitem/index.js
+++ src/components/linkitem/index.js
@@ -4,7 +4,7 @@ import styles from './index.module.css'
 export function LinkItem({title = "please provide a link object.", description = '', href = "https://github.com/sord-dev"}) {
   const titleClass = description ? '' : styles.center;
   return (
-    <a className={styles.linkItem} href={href} about="_blank" rel='noreferrer noopener' >
+    <a className={styles.linkItem} href={href} target="_blank" rel='noreferrer noopener' >
       <h3 className={titleClass}>{title}</h3>
       {description && <p>{description}</p>}
     </a>
