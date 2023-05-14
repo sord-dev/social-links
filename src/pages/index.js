672f438031404b4414f7551fdb09df48d206d35a
sord-dev <75338985+sord-dev@users.noreply.github.com>
2023-05-14 12:42:01 +0100

    add config file + better sai thumbnail

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,53 +11,8 @@ import { useContext } from "react";
 import { ModalContext } from "../utils/contexts/modalContext";
 import styles from "../styles/Home.module.css";
 
-const links = [
-  { src: "https://github.com/sord-dev", tag: "Github" },
-  { src: "https://linkedin.com/in/stefansyrett/", tag: "LinkedIn" },
-  { src: "mailto:stefansyrett17@gmail.com", tag: "Email" },
-  { src: "https://docs.google.com/document/d/1YgO4AhYtWiec13T5Vxv4zgl8o1S-_MzFJfERLmmYk0Q/edit?usp=sharing", tag: "CV" },
-  { src: "http://www.patience-is-a-virtue.org", tag: "Discord" },
-];
-
-const projects = [
-  {
-    id: "c740bec6-a3bc-11ed-a8fc-0242ac120002",
-    tech: "Nextjs, TypeScript, Google Books API",
-    thumbnail: "https://i.postimg.cc/gJ4npY3W/value-books.png",
-    name: "High Value Books",
-    description:
-      "A repository with all the books I've seen recommended by successful people. With searching and recommendation capabilities.",
-    code: "https://github.com/sord-dev/adonislibrary",
-    websiteUrl: "https://adonislibrary.vercel.app",
-  },
-  {
-    id: "c740c8ee-a3bc-11ed-a8fc-0242ac120002",
-    tech: "Nextjs, JavaScript, framer-motion",
-    thumbnail: "https://i.postimg.cc/D0Zmytcb/makeup-portfolio.png",
-    name: "Makeup Artist Portfolio",
-    description:
-      "Makeup Artist portfolio created for Freelance work, showcasing the makeup and photoshoots the client has done.",
-    code: "https://github.com/sord-dev/Ashleigh-Lavery",
-    websiteUrl: "https://ashleigh-lavery.vercel.app",
-  },
-  {
-    id: "c740cace-a3bc-11ed-a8fc-0242ac120002",
-    tech: "React, JavaScript, Design, OpenAI, Team Project",
-    thumbnail: "https://i.ibb.co/5hDg51t/sai-dashboard.png",
-    name: "SerenityAI",
-    description:
-      "A mental health application catered towards people who struggle with accountability and people who just want to hear a second opinion from a guide/therapist-like AI.",
-    code: "https://github.com/sord-dev/mental-health-server",
-    websiteUrl: "https://serenity-ai.netlify.app/authenticate/register",
-  },
-];
-
-const SEOConfig = {
-  title: "Stefan Syrett | ReactJs Developer",
-  description:
-    "Just a developer/designer trying to provide value! I enjoy solving business and specific problems, and strive to become better at doing so continually.",
-  icon: "/bitmoji-wink.png",
-};
+import data from '../utils/config.json'
+const { links, projects, seoConfig } = data;
 
 export default function Home() {
   const { modalActive, modalCard } = useContext(ModalContext);
@@ -65,7 +20,7 @@ export default function Home() {
   return (
     <ParticleBackground>
       <FadeInAnimation>
-        <SEO {...SEOConfig} />
+        <SEO {...seoConfig} />
         <div className={styles.container}>
           {modalActive && <Modal {...{ modalCard }} />}
           <div className={styles.main}>
